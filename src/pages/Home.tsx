00077d6c94a62c8dfcb69215bf6803352a901909
Mateus Santos <54743403+mateusdata@users.noreply.github.com>
2024-02-19 14:54:54 +0000

    feat: Login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { ContextAuth } from '../context/AuthProvider'
 
 const Home = () => {
   const { user, setUser } = useContext(ContextAuth);
-  const sair = () => {
+  const SignOut = () => {
     try {
       localStorage.removeItem("user");
       setUser(null);
@@ -14,7 +14,7 @@ const Home = () => {
   return (
     <div className='flex justify-center items-center h-screen w-full flex-col gap-3 text-center'>
      <span> {`Bem vindo ${user?.name}, vc está logado. seu email é ${user?.email}`}</span>
-      <button onClick={sair}
+      <button onClick={SignOut}
         className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full 
          border border-transparent bg-blue-600 text-white hover:bg-blue-700
           disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none
