148d86bb021e8c81eec3bd89c4b84278068363f0
Mateus Santos <mateuspele2015@gmail.com>
2024-02-19 13:30:29 -0300

    fix: Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,8 @@ const Home = () => {
   }
   return (
     <div className='flex justify-center items-center h-screen w-full flex-col gap-3 text-center'>
-     <span> {`Bem vindo ${user?.name}, vc está logado. seu email é ${user?.email}`}</span>
+      <h1>Home</h1>
+    {false && <span> {`Bem vindo ${user?.name}, vc está logado. seu email é ${user?.email}`}</span>}
       <button onClick={signOut}
         className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full 
          border border-transparent bg-blue-600 text-white hover:bg-blue-700
